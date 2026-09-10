ecff87fc911a7d18bb941edfef70a3fef6965eb6
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-10 08:58:40 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -4,6 +4,4 @@ exercises_completed + 13
 points_per_exercises = 5
 
 point_total = 100
-point_total +=exercises_completed * points_per_exercises
-
-point("I got "+str(point_total_) +"points!")
+point_total +=exercises_completed * points_per_exerci
