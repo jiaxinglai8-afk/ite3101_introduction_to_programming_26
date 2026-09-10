37c3df43bce75e87519a591d89c9e2e9d0eb382e
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-10 07:30:21 +0000

    [skip actions] lab/lab01/ch01_t02_print_statements.py

--- lab/lab01/ch01_t02_print_statements.py
+++ lab/lab01/ch01_t02_print_statements.py
@@ -1,3 +1,3 @@
-print("Water—there is not a drop of water there! Were Niagara but a cataract of sand, would you travel your thousand miles to see it?")
+
 print('Mismatched quotes will cause a SyntaxError')
 print('Without quotes will cause a NameError')
