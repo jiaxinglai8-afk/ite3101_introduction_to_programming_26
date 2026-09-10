a480dd10accf0c3a48fff9dc40c0971aa3d9ea3b
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-10 09:03:06 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,9 +1,9 @@
 skill_completed = "Paython Syntax"
 exercises_completed = 13
 
-points_per_exercises = 5
+points_per_exercise = 5
 
 point_total = 100
-point_total +=exercises_completed * points_per_exercises
+point_total +=exercises_completed * points_per_exercise
 
 print("I got" + str(point_total) + "points!")
