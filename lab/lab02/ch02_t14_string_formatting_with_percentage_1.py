6c5efbdf62250734434669739a39fca68eba792d
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-17 08:41:24 +0000

    [skip actions] lab/lab02/ch02_t14_string_formatting_with_percentage_1.py

--- lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
+++ lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
@@ -2,5 +2,5 @@ string_1 = "Camelot"
 string_2 = "place"
 
 #print("Let's not go to %s. 'Tis a silly %s." % (string_1,stringh_2))
-print(f"Let's not go to %s. 'Tis a silly %s." % (string_1,stringh_2))
+print(f"Let's not {go to %s. 'Tis a silly %s." % (string_1,stringh_2))
 
