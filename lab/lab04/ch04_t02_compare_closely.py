6dbf6bd0e1857df4edfde2405e2d200590f80d32
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-24 07:34:47 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -1,6 +1,6 @@
 # Assign True or False as appropriate on the lines below!
 
-# (20-10et this to True if 17 < 328 or to False if it is not.
+et this to True if 17 < 328 or to False if it is not.
 bool_one = False  # We did this one for you!
 
 # Set this to True if 100 == (2 * 50) or to False otherwise.
