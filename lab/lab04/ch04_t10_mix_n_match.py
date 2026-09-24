83fc5b0c4160762c34e1d3a1e457f712d23804ac
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-24 07:46:41 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -4,13 +4,13 @@
 bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 
 # Make me true!
-bool_two = (2 <= 2) or
+bool_two = (2 <= 2) or "Alpha" == "Bravo"
 
 # Make me false!
-bool_three = (2 <= 2) and
+bool_three = (2 <= 2) and 
 
 # Make me true!
-bool_four = (2 <= 2)
+bool_four = (2 <= 2) or
 
 # Make me true!
-bool_five =(2 <= 2)
+bool_five =(2 <= 2) or
