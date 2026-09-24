616caf39b18eb56663eab9f70616913f83dc6e4b
jiaxinglai8-afk <jiaxinglai8@gmail.com>
2026-09-24 08:29:25 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -4,7 +4,7 @@ def grade_converter(grade: int) -> str:
         return "A"
     elif grade >= 80:
         return "B"
-    elif grade >70:
+    elif grade >= 70:
         return "C"
     elif grade:
         return "D"
